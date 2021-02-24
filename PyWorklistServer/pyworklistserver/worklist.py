3ecc9ba573c246a870ad97ceb2cb6fc3d7d6231b
Jelnes <78155293+Jelnes@users.noreply.github.com>
2021-02-24 10:48:28 +0100

    Add files via upload

---
+++ PyWorklistServer/pyworklistserver/worklist.py
@@ -0,0 +1,95 @@
+""" Worklist generator classes """
+
+import datetime
+import random
+import string
+from pydicom.uid import generate_uid
+from pydicom.dataset import Dataset
+
+__NONASCII = 'æÆøØåÅßäöüÄÖÜ' # Just an arbitrarily selected list of non ascii characters
+
+def _random_unicode_string(length):
+    """ Create a random string of specified length containing some non-ascii characters """
+    return ''.join(random.choices(' ' + string.ascii_uppercase + string.ascii_lowercase + string.digits + __NONASCII, k=length))
+
+def _extend_with_random_to_length(text, length):
+    """ Extend a string with random characters up to the given length """
+    return text + _random_unicode_string(length - len(text))
+
+def _random_person_name(max_len_per_name):
+    """ Create a random person name separated by ^ character """
+    return '^'.join([_random_unicode_string(max_len_per_name) for _ in range(1, random.randrange(2, 4))])
+
+def _random_number_string(max_len):
+    """ Create a random string containing numeric values only """
+    return ''.join(random.choices(string.digits, k=random.randrange(1, max_len)))
+
+def _random_dicom_date_after_1900():
+    """ Return a string containing a random date on DICOM format """
+    start = datetime.date(1900, 1, 1)
+    now = datetime.date.today()
+    maxdays = (now - start)
+    randomdays = random.randrange(0, maxdays.days)
+    date = start + datetime.timedelta(days=randomdays)
+    return date.strftime('%Y%m%d')
+
+def _random_dicom_time():
+    """ Return a string containing a random time point on DICOM format """
+    hour = random.randrange(0, 24)
+    minutes = random.randrange(0, 59)
+    seconds = random.randrange(0, 59)
+    return '{:02}{:02}{:02}'.format(hour, minutes, seconds)
+
+_VIVID_HACK_MAX_PERSON_NAME = 64
+
+class RandomWorklist:
+    """ Generator for random worklists """
+
+    def __init__(self, specificCharSet):
+        self._specific_charset = specificCharSet
+
+    def get_worklist(self):
+        """ Generate a random worklist with random number of worklist items """
+        worklist = []
+        num_items = random.randrange(1, 100)
+        for i in range(0, num_items):
+            worklist_item = Dataset()
+            worklist_item.StudyInstanceUID = generate_uid('1.2.840.113619.2.391.6789.')
+            worklist_item.Modality = 'US'
+            worklist_item.SpecificCharacterSet = self._specific_charset
+            worklist_item.AccessionNumber = _random_unicode_string(16)
+            worklist_item.PatientBirthDate = _random_dicom_date_after_1900()
+            worklist_item.PatientName = self._get_person_name()
+            worklist_item.PatientID = _extend_with_random_to_length('Patient id ', 64)
+            worklist_item.IssuerOfPatientID = _extend_with_random_to_length('Issuer of patient id ', 64)
+            worklist_item.PatientWeight = str(random.uniform(10.0, 150.0))[:16]
+            worklist_item.PatientSize = str(random.uniform(1.0, 2.5))[:16]
+            worklist_item.AdmissionID= _extend_with_random_to_length('Admission id ', 64)
+            worklist_item.RequestedProcedureID = _extend_with_random_to_length('Step id ', 16)
+            worklist_item.RequestedProcedureDescription = _extend_with_random_to_length('Step description ', 64)
+
+            otherPatientIdsSq = [Dataset(), Dataset()]
+            for otherPatientId in otherPatientIdsSq:
+                otherPatientId.PatientID = _extend_with_random_to_length('Other patient id ', 64)
+                otherPatientId.IssuerOfPatientID = _extend_with_random_to_length('Issuer of patient id ', 64)
+                otherPatientId.TypeOfPatientID = 'TEXT'
+
+            worklist_item.OtherPatientIDsSequence = otherPatientIdsSq
+
+            step = Dataset()
+            step.ScheduledPerformingPhysicianName = self._get_person_name()
+            step.ScheduledProcedureStepStartDate = _random_dicom_date_after_1900()
+            step.ScheduledProcedureStepStartTime = _random_dicom_time()
+            step.ScheduledProcedureStepDescription = _extend_with_random_to_length('Scheduled procedure step desc ', 64)
+            step.CommentsOnTheScheduledProcedureStep = _extend_with_random_to_length('Scheduled step comments ', 10240)
+            worklist_item.ScheduledProcedureStepSequence = [step]
+
+            # Other patient ID sequence patientid issuer of patient id
+
+            worklist.append(worklist_item)
+        return worklist
+
+    def _get_person_name(self):
+        """ Create a random person name and truncate the name components according to Vivid bug """
+        return _random_person_name(32)[:_VIVID_HACK_MAX_PERSON_NAME]  # fix for wrong handling in EchoPAC/Scanner
+    
---
+++ PyWorklistServer/pyworklistserver/worklist_server.py
@@ -0,0 +1,52 @@
+""" DICOM server implementation that responds to C-FIND requests"""
+from pynetdicom import AE
+from pynetdicom import evt
+from pynetdicom.sop_class import ModalityWorklistInformationFind
+
+from pyworklistserver import worklist
+
+class WorklistServer:
+    """ Simple worklist server with support for one client. This
+    implementation class is responsible for network communication with
+    the client. """
+
+    def __init__(self, serverConfig, app_logger, blocking):
+        self._config = serverConfig
+        self._logger = app_logger
+        self._blocking = blocking
+        self._worklist_factory = worklist.RandomWorklist('ISO_IR 100')
+        self._handlers = [
+            (evt.EVT_C_FIND, self._on_find, [app_logger]),
+        ]
+        self._server = None
+
+    def start(self):
+        """ Start the server and listen to specified address and port """
+        ae = AE(self._config.ae_title)
+        ae.add_supported_context(ModalityWorklistInformationFind)
+        self._logger.info('Running worklist server with AE title {}, ip: {}, listening to port: {}'.format(
+            self._config.ae_title, 
+            self._config.network_address.address,
+            self._config.network_address.port)
+        )
+
+        self._server = ae.start_server(
+            self._config.network_address, 
+            evt_handlers=self._handlers, 
+            block=self._blocking)
+
+    def stop(self):
+        """ Stop the server """
+        self._logger.info('Shutting down')
+        self._server.shutdown()
+
+    def _on_find(self, event, app_logger):
+        """ Event handler for C-FIND requests """
+        worklist_items = self._worklist_factory.get_worklist()
+        app_logger.info('Created worklist with {} exams'.format(len(worklist_items)))
+        for worklist_item in worklist_items:
+            if event.is_cancelled:
+                yield (0xFE00, None)  # Check if C-CANCEL has been received
+                return
+            yield (0xFF00, worklist_item)
+        return
