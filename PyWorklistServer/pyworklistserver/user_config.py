12cc0aea794519f64936f0d0605daa531c9aa51f
Mollefunken <tomas.godo@hotmail.com>
2021-03-10 11:01:02 +0100

    update of user_config names

--- PyWorklistServer/pyworklistserver/user_config.py
+++ PyWorklistServer/pyworklistserver/user_config.py
@@ -2,10 +2,10 @@
 import random
 
 #Maximall number of total patient-objects delivered from generator.
-maxAmountOfPatientObjects = 500     
+maxAmountOfWorklistExams = 500
 
 #Rate of clean patient-objects (in percentage) delivered from generator.
-rateOfCleanObjects = 20
+rateOfCleanExams = 20
 
 #Generator seed value, if command line argument not set. Set to 0 for seed to be set to a random number.
 seed_Number = 0
