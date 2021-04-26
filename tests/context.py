8be33b9094088ff7de74e9c1b896e10a5005e939
Peder Ellefsen <78155209+pedellefsen@users.noreply.github.com>
2021-04-26 11:40:16 +0200

    Add files via upload

---
+++ tests/context.py
@@ -0,0 +1,6 @@
+""" Workaround to set path correctly for tests """
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pyworklistserver
