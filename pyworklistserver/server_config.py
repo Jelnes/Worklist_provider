5c6a77108d9c8855154d97744bdb861ff7e44372
Peder Ellefsen <peder.faerestrand@gmail.com>
2021-04-26 11:37:47 +0200

    Changes to be committed: 	deleted:    PyWorklistServer.pyproj 	deleted:    PyWorklistServer.sln 	deleted:    build.proj 	deleted:    pyworklistserver/__init__.py 	deleted:    pyworklistserver/__main__.py 	deleted:    pyworklistserver/__pycache__/__init__.cpython-37.pyc 	deleted:    pyworklistserver/__pycache__/server_config.cpython-37.pyc 	deleted:    pyworklistserver/__pycache__/worklist.cpython-37.pyc 	deleted:    pyworklistserver/__pycache__/worklist_server.cpython-37.pyc 	deleted:    pyworklistserver/server_config.py 	deleted:    pyworklistserver/worklist.py 	deleted:    pyworklistserver/worklist_server.py 	deleted:    readme.md 	deleted:    requirements.txt 	deleted:    tests/__pycache__/context.cpython-37.pyc 	deleted:    tests/__pycache__/tls_util.cpython-37.pyc 	deleted:    tests/__pycache__/worklist_client.cpython-37.pyc 	deleted:    tests/context.py 	deleted:    tests/worklist_client.py 	deleted:    tests/worklist_server_tests.py

--- pyworklistserver/server_config.py
+++
@@ -1,5 +0,0 @@
-""" Structs/tuples used across the package """
-from collections import namedtuple
-
-NetworkAddress = namedtuple('NetworkAddress', ['address', 'port'])
-ServerConfig = namedtuple('ServerConfig', ['network_address', 'ae_title', 'verbose'])
